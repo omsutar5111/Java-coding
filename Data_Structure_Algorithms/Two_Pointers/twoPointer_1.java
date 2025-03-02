7f224877b907c0429d37055ad8fa25ad40e5d21c
omkarsutar3 <omsutar5111@gmail.com>
2025-03-02 14:50:31 +0530

    Leetcode problem solved

--- Data_Structure_Algorithms/Two_Pointers/twoPointer_1.java
+++ Data_Structure_Algorithms/Two_Pointers/twoPointer_1.java
@@ -22,6 +22,7 @@ public class twoPointer_1 {
         return count % mod;
     }
 
+     //Leetcode 2570
     public int[][]mergeSorted(int[][] nums1, int[][] nums2){
         int i=0,j=0;
         List<int []>ans=new ArrayList<>();
@@ -48,6 +49,25 @@ public class twoPointer_1 {
             j++;
         }
         return ans.toArray(new int[ans.size()][]);
+    }
+       //Leetcode 2460
+    public int []applyOperations(int[]nums){
+        for(int i=0;i< nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]*=2;
+                nums[i+1]=0;
+            }
+        }
+        int p1=0;
+        for(int i=0;p1< nums.length;i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[p1];
+                nums[p1]=temp;
+                p1++;
+            }
+        }
+        return nums;
     }
 
     public static void main(String[] args) {
