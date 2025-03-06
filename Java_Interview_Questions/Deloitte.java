6231b371027a8b3d31a7fc227fe999a77d7ac421
omkarsutar3 <omsutar5111@gmail.com>
2025-03-06 15:20:58 +0530

    Interview package created

--- Java_Interview_Questions/Deloitte.java
+++ Java_Interview_Questions/Deloitte.java
@@ -4,7 +4,25 @@ import java.util.*;
 
 public class Deloitte {
   public static void result(int [][]values,int rows,int cols){
-
+     for(int col=0;col<cols;col++){
+         int maxVal=Integer.MIN_VALUE;
+         for(int row=0;row<rows;row++){
+             if(values[row][col]!=-1){
+                 maxVal=Math.max(values[row][col],maxVal);
+             }
+         }
+         for(int row=0;row<rows;row++){
+             if(values[row][col]==-1){
+                 values[row][col]=maxVal;
+             }
+         }
+     }
+     for(int i=0;i<rows;i++){
+         for(int j=0;j<cols;j++){
+             System.out.print(values[i][j]+ " ");
+         }
+         System.out.println();
+     }
   }
   public static  String mostOccuringFirst(String input){
       if (input == null || input.isEmpty()) {
@@ -35,5 +53,12 @@ public class Deloitte {
     public static void main(String[] args) {
         String input = "apple banana apple orange banana apple orange";
         System.out.println("Most occurred first word: " + mostOccuringFirst(input));
+        int[][] values = {
+                {3, -1, 5},
+                {1, 9, -1},
+                {4, 2, 6}
+        };
+
+        result(values,3,3);
     }
 }
